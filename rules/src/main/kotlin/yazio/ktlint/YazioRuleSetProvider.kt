862a1020d43fc7ed4302d51b78b85e7eb379c543
Paul Woitaschek <woitaschek@gmail.com>
2022-11-02 14:26:42 +0100

    Setup the ruleset provider.

---
+++ rules/src/main/kotlin/yazio/ktlint/YazioRuleSetProvider.kt
@@ -0,0 +1,19 @@
+package yazio.ktlint
+
+import com.pinterest.ktlint.core.RuleProvider
+import com.pinterest.ktlint.core.RuleSetProviderV2
+
+class YazioRuleSetProvider : RuleSetProviderV2(
+  id = "yazio",
+  about = About(
+    maintainer = "YAZIO",
+    description = "YAZIO ktlint rules",
+    license = "https://github.com/yazio/YazioKtlintRules/blob/main/LICENSE",
+    repositoryUrl = "https://github.com/yazio/YazioKtlintRules",
+    issueTrackerUrl = "https://github.com/yazio/YazioKtlintRules/issues",
+  )
+) {
+  override fun getRuleProviders(): Set<RuleProvider> {
+    return setOf(RuleProvider { NonSuspendingFlowFunctionRule() })
+  }
+}
